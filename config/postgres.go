d3b32e9bd722d31822e45b889ccf6d6f3a116d4b
Petagonest <81139813+Petagonest@users.noreply.github.com>
2021-12-11 22:51:38 +0700

    FIX

--- config/postgres.go
+++ config/postgres.go
@@ -3,24 +3,24 @@ package config
 import (
 	"database/sql"
 	"fmt"
-	// "log"
+	"log"
 	"os"
 	"strconv"
 
-	// "github.com/joho/godotenv"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 func PembuatanKoneksi() (*sql.DB, error) {
-	// err := godotenv.Load(".env")
+	err := godotenv.Load(".env")
 
-	// if err != nil {
-	// 	log.Fatalf("Error loading .env file")
-	// }
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
 
 	port, err := strconv.Atoi(os.Getenv("DB_PORT"))
 
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=require",
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable",
 		os.Getenv("DB_HOST"), port, os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
 
 	db, err := sql.Open("postgres", psqlInfo)
